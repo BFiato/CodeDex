e7aeb96e126951fd4a0585d7890a5d04d6b928b4
BFiato <152137708+BFiato@users.noreply.github.com>
2024-06-21 14:00:09 -0400

    8Ball.py

--- 8Ball.py
+++ 8Ball.py
@@ -1,5 +1,8 @@
-import random
-eightball = ['Yes - definitely.', 'It is decidedly so.', 'Without a doubt.', 'Reply hazy, try again.', 'Ask again later.', 'Better not tell you now.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.'  ];
-input('Question: ')
-num = random.randint(0, 8)
-print('Magic 8 Ball: ', eightball[num]);
+rpt = 'yes'
+while (rpt != 'no'):
+  import random
+  eightball = ['Yes - definitely.', 'It is decidedly so.', 'Without a doubt.', 'Reply hazy, try again.', 'Ask again later.', 'Better not tell you now.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.'  ];
+  input('Question: ')
+  num = random.randint(0, 8)
+  print('Magic 8 Ball: ', eightball[num])
+  rpt = input('Do you have another question? ')
