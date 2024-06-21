b87637a1218e45bd252b1a9c3d76645c67e54ac7
BFiato <152137708+BFiato@users.noreply.github.com>
2024-06-21 12:18:40 -0400

    8Ball.py

---
+++ 8Ball.py
@@ -0,0 +1,5 @@
+import random
+eightball = ['Yes - definitely.', 'It is decidedly so.', 'Without a doubt.', 'Reply hazy, try again.', 'Ask again later.', 'Better not tell you now.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.'  ];
+input('Question: ')
+num = random.randint(0, 8)
+print('Magic 8 Ball: ', eightball[num]);
